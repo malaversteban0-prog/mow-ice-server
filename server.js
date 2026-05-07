37dc1788337281f4843818f94f91c0a44b8db365
steban <malaversteban0@gmail.com>
2026-05-06 23:45:02 -0500

    frontend online

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static(__dirname));
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,

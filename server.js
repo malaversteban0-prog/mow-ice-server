c550eca370815b3fac9e055d51fa0dceb6db2f0a
steban <malaversteban0@gmail.com>
2026-05-06 23:30:27 -0500

    rutas datos

--- server.js
+++ server.js
@@ -1,80 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
-require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+let database = {
+    productos: [],
+    historial: [],
+    ventasActuales: []
+};
 
 app.get('/', (req, res) => {
-  res.send('MOW ICE ONLINE');
+    res.send('MOW ICE ONLINE');
 });
 
-app.get('/productos', async (req, res) => {
-  try {
-    const result = await pool.query(
-      'SELECT * FROM productos ORDER BY id DESC'
-    );
-
-    res.json(result.rows);
-
-  } catch(err) {
-    console.log(err);
-    res.status(500).json({
-      error: 'error'
-    });
-  }
+app.get('/datos', (req, res) => {
+    res.json(database);
 });
 
-app.post('/productos', async (req, res) => {
+app.post('/guardar', (req, res) => {
 
-  try {
+    database = req.body;
 
-    const {
-      nombre,
-      precio,
-      stock,
-      img,
-      costos
-    } = req.body;
+    console.log('DATOS GUARDADOS');
 
-    const result = await pool.query(
-      `INSERT INTO productos
-      (nombre, precio, stock, img, costos)
-      VALUES ($1,$2,$3,$4,$5)
-      RETURNING *`,
-      [
-        nombre,
-        precio,
-        stock,
-        img,
-        JSON.stringify(costos)
-      ]
-    );
-
-    res.json(result.rows[0]);
-
-  } catch(err) {
-    console.log(err);
-
-    res.status(500).json({
-      error: 'error'
+    res.json({
+        ok: true
     });
-  }
 
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Servidor funcionando');
+    console.log('SERVIDOR ONLINE ' + PORT);
 });
